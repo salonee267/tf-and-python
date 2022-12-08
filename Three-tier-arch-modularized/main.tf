f74790458da125ddc5089abdb7b94c4eb3a764ec
salonee267 <62054024+salonee267@users.noreply.github.com>
2022-12-08 10:13:58 +0530

    Update main.tf

--- Three-tier-arch-modularized/main.tf
+++ Three-tier-arch-modularized/main.tf
@@ -9,65 +9,9 @@ module "aws_vpc" {
   source = "./modules/networking_comp" 
 }
 
-module "aws_subnet" {
-  source = "./modules/networking_comp"
-}
-
-module "aws_internet_gateway" {
-  source = "./modules/networking_comp"
-}
-
-module "aws_route_table" {
-  source = "./modules/networking_comp"
-}
-
-module "aws_eip" {
-  source = "./modules/networking_comp"
-}
-
-module "aws_nat_gateway" {
-  source = "./modules/networking_comp"
-}
-
-module "aws_db_subnet_group" {
-  source = "./modules/rds"
-  private_subnets = var.private_subnets
-}
-
 module "aws_db_instance" {
   source = "./modules/rds"
   private_subnets = var.private_subnets
 }
-
-module "aws_security_group" {
-  source = "./modules/networking_comp"
-  // vpc_id = var.vpc_id
-}
-
-module "aws_instance" {
-  source = "./modules/networking_comp"
-  // vpc_id = var.vpc_id
-}
-
-module "aws_lb" {
-  source = "./modules/networking_comp"
-  // vpc_id = var.vpc_id
-}
-
-module "aws_lb_target_group" {
-  source = "./modules/networking_comp"
-  // vpc_id = var.vpc_id
-}
-
-module "aws_lb_listener" {
-  source = "./modules/networking_comp"
-  // vpc_id = var.vpc_id
-}
-
-module "aws_lb_listener_rule" {
-  source = "./modules/networking_comp"
-  // vpc_id = var.vpc_id
-}
-
 
 
