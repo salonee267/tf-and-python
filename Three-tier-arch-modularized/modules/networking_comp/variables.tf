637dd5cc1b4410fe34212c26fd38bf06c60d96b7
salonee267 <62054024+salonee267@users.noreply.github.com>
2022-12-08 10:51:03 +0530

    Update variables.tf

--- Three-tier-arch-modularized/modules/networking_comp/variables.tf
+++ Three-tier-arch-modularized/modules/networking_comp/variables.tf
@@ -18,20 +18,20 @@ variable "public_subnets_cidr_blocks" {
   default     = ["10.0.5.0/24", "10.0.7.0/24"]
 }
 
-variable "web_subnets_cidr_blocks" {
+variable "private_subnets_cidr_blocks" {
   description = "CIDR blocks of subnets in web layer"
   default     = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
-variable "app_subnets_cidr_blocks" {
-  description = "CIDR blocks of subnets in app layer"
-  default     = ["10.0.2.0/24", "10.0.4.0/24"]
-}
+// variable "app_subnets_cidr_blocks" {
+//   description = "CIDR blocks of subnets in app layer"
+//   default     = ["10.0.2.0/24", "10.0.4.0/24"]
+// }
 
-variable "db_subnets_cidr_blocks" {
-  description = "CIDR blocks of subnets in DB layer"
-  default     = ["10.0.6.0/24", "10.0.8.0/24"]
-}
+// variable "db_subnets_cidr_blocks" {
+//   description = "CIDR blocks of subnets in DB layer"
+//   default     = ["10.0.6.0/24", "10.0.8.0/24"]
+// }
 
 variable "websg_name" {
   description = "Name of security group for webservers"
