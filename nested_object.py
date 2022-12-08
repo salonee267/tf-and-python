c0d574da1062ad8951e429ed9cad41b4527fa64f
salonee267 <62054024+salonee267@users.noreply.github.com>
2022-12-08 09:42:44 +0530

    Update nested_object.py

--- nested_object.py
+++ nested_object.py
@@ -1,32 +1,27 @@
-from socket import setdefaulttimeout
-
 
 def functiontest(dictionary,key_list):
     if len(dictionary) == 0:
         print('Dictionary is empty')
     if len(key_list) == 0:
-    	print("Invalid Key Supplied")
+        print("Invalid Key Supplied")
     key_to_search = key_list[0]
     if key_to_search in dictionary.keys():
-    	value_found = dictionary[key_to_search]
-    	if type(value_found) is dict:
-    		functiontest(value_found, key_list[1:])
-    	elif len(key_list) == 1:
-      	    print(value_found)
-    	else:
-    	    print (f"Invalid key {key_list}")
+        value_found = dictionary[key_to_search]
+        if type(value_found) is dict:
+            functiontest(value_found, key_list[1:])
+        elif len(key_list) == 1:
+            print(value_found)
+        else:
+            print (f"Invalid key {key_list}")
     else:
-    	print(f"Key {key_to_search} not found in dictionary {dictionary}")
-
-                
-                
-            
-
+        print(f"Key {key_to_search} not found in dictionary {dictionary}")
+ 
+ 
+ 
+ 
+ 
 if __name__=='__main__':
-    sample_dict={}
-    # create nested dictionary
-    sample_dict.setdefault("DiGi",{}).setdefault("age",'26')
-    sample_dict.setdefault("Lola",{}).setdefault("age",'22')
-    #key value (in string format) to check in dictionary
-    key_value=["Lola",'age', 'hobbie']
-    functiontest(sample_dict,key_value)
+    sample_dict = {"x":{"y":{"z":"a"},"p":{"q":"r"}, "j" : "t"}, "d":{"g":{"s":"p"}}}
+ 
+    key_value = ["x", "y", "z"]
+    functiontest(sample_dict, key_value)
